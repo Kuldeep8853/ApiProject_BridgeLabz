144453cba8759edeeb4234dafe429cfac487e203
stru8854 <60762201+stru8854@users.noreply.github.com>
2020-03-28 13:30:32 +0530

    [Kuldeep]Refactor. Add Repository and Manager library class

--- EmpManagment/EmpCRUD/Startup.cs
+++ EmpManagment/EmpCRUD/Startup.cs
@@ -6,6 +6,7 @@ using EmployeeModel;
 using ManagerClass;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -13,6 +14,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Repository;
 using System.Configuration;
+//using Microsoft.AspNetCore.Mvc;
 
 namespace EmpCRUD
 {
@@ -26,19 +28,32 @@ namespace EmpCRUD
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+           
+        //    services.AddDbContext<EmployeeContext>(option => option.UseSqlServer(Configuration.GetConnectionString("UserDbConnection")));
+        //    services.AddMvcCore().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        //    services.AddTransient<IManager, ImpManager>();
+        //    services.AddTransient<IEmployee, ImpEmployee>();
+
+        //    services.AddSwaggerGen(c =>
+        //    {
+        //        c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+        //    });
+        //}
         public void ConfigureServices(IServiceCollection services)
         {
-           
-            //services.AddDbContext<EmployeeContext>(option => option.UseSqlServer(configuration.GetConnectionString("UserDbConnection")));
+            services.AddDbContextPool<EmployeeContext>(options => options.UseSqlServer(Configuration.GetConnectionString("UserDbConnection")));
             services.AddMvcCore().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddTransient<IManager, ImpManager>();
             services.AddTransient<IEmployee, ImpEmployee>();
-
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
+
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
--- EmpManagment/Manager/EmployeeContext .cs
+++ EmpManagment/Manager/EmployeeContext .cs
@@ -4,10 +4,14 @@ namespace EmployeeModel
 {
     public class EmployeeContext : DbContext
     {
+        public EmployeeContext(DbContextOptions options) : base(options)
+        {
+
+        }
         public DbSet<Employee> Employees { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       /* protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True");
-        }
+        }*/
     }
 }

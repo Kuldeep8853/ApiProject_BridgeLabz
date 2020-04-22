39d3d6b4fab3b609c21747e1a0054388801aee48
Kuldeep8853 <kkasaudhan8853@gmail.com>
2020-04-22 07:06:23 +0530

    [Kuldeep]Refactor. Modifie the code

--- quntiry_measurement_ui_app/src/App.js
+++ quntiry_measurement_ui_app/src/App.js
@@ -5,22 +5,20 @@ import Length from './Pages/Length';
 import Temprature from './Pages/Temprature';
 import Weight from './Pages/Weight';
 import { QuantityRow } from './component/QuantityRow';
-import Head from './Pages/Head';
 
 function App() {
   const Menu = withRouter(QuantityRow);
   return (
     <div className="App">
       <div className="optionColor">
-        <h1>Quantity Measurment</h1>
+        <h1>Quantity Measurement</h1>
         <BrowserRouter>
         <Menu/>
       <div>
         <Switch>
-          <Route path='/' component={Head} exact/>
         <Route path='/Length' component={Length} exact />
-        <Route path='/Weight' component={Weight} exact />
-        <Route path='/Temprature' component={Temprature} exact/>
+        <Route path='/Weight' component={Weight}  />
+        <Route path='/Temprature' component={Temprature} />
         </Switch>
       </div>
     </BrowserRouter>  
--- quntiry_measurement_ui_app/src/Pages/Head.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Head() {
-    return (
-        <div>
-            <h1>This is Quantity Measurment page.</h1>
-        </div>
-    )
-}

a6c7c3e0532adae1fe9d2ff468e88a135d9c2507
Kuldeep8853 <kkasaudhan8853@gmail.com>
2020-04-27 17:16:51 +0530

    [Kuldeep]Add. Create parking project

---
+++ ParkingLot_Problem/Model/Parking.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+    public class ParkingModel
+    {
+        [Key]
+        public int ParkingSlotNo { get; set; }
+
+        [Required]
+        public string VehicalNo { get; set; }
+
+        [Required]
+        public string VehicalType { get; set; }
+
+        public int ChargesPerHour { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        [Required]
+        public DateTime EntryTime { get; set; }
+
+        [Required]
+        public DateTime OutingTime { get; set; }
+
+        [Required]
+        public string ParkingType { get; set; }
+    }
+}
---
+++ ParkingLot_Problem/Repository/UserContext/UserContextDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.UserContext
+{
+    public class UserContextDB:DbContext 
+    {
+
+        public UserContextDB( DbContextOptions<UserContextDB> options) : base(options)
+        {
+        }
+        public DbSet<Parking> ParkingSpace { set; get; }
+    }
+}

b4396007d70331e9f76803e552114d4b3c88b177
Kuldeep8853 <kkasaudhan8853@gmail.com>
2020-05-01 06:27:18 +0530

    [Kuldeep]Add. Add the security Manager

--- ParkingLot_Problem/Manager/SecurityManager/ISecurityManager.cs
+++ ParkingLot_Problem/Manager/SecurityManager/ISecurityManager.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Manager.SecurityManager
 {
-    interface ISecurityManager
+    public interface ISecurityManager
     {
+        Task<int> Parkking(Parking parking);
+        string UnParking(int parkingSlotId);
     }
 }
--- ParkingLot_Problem/Manager/SecurityManager/SecurityManager.cs
+++ ParkingLot_Problem/Manager/SecurityManager/SecurityManager.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Model;
+using Repository.SecurityRepository;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Manager.SecurityManager
 {
-    class SecurityManager
+    public class SecurityManager : ISecurityManager
     {
+        private readonly ISecurityRepository securityRepository;
+
+        public SecurityManager(ISecurityRepository securityRepository)
+        {
+            this.securityRepository = securityRepository;
+        }
+
+        public Task<int> Parkking(Parking parking)
+        {
+           return this.Parkking(parking);
+        }
+
+        public string UnParking(int parkingSlotId)
+        {
+           return this.UnParking(parkingSlotId);
+        }
     }
 }

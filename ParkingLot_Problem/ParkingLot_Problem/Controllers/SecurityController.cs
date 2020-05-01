fb1b60175108f7371948c437c4969444da28ce4d
Kuldeep8853 <kkasaudhan8853@gmail.com>
2020-05-01 06:33:11 +0530

    [Kuldeep]Add. Add the security Controller

--- ParkingLot_Problem/ParkingLot_Problem/Controllers/SecurityController.cs
+++ ParkingLot_Problem/ParkingLot_Problem/Controllers/SecurityController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Manager.SecurityManager;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Model;
 
 namespace ParkingLot_Problem.Controllers
 {
@@ -11,5 +13,30 @@ namespace ParkingLot_Problem.Controllers
     [ApiController]
     public class SecurityController : ControllerBase
     {
+        private readonly ISecurityManager securityManager;
+
+        public SecurityController(ISecurityManager securityManager)
+        {
+            this.securityManager = securityManager;
+        }
+
+        [Route("ParkVahical")]
+        [HttpPost]
+        public async Task<IActionResult> Parking_Vahical(Parking parking)
+        {
+            object result = await this.securityManager.Parkking(parking);
+            if (result != null)
+                return this.Ok(parking);
+
+            return this.BadRequest();
+        }
+
+        [Route("UnParkVahical")]
+        [HttpDelete]
+        public string UnParking_Vahical(int ParkingSlotId)
+        {
+            string result = this.securityManager.UnParking(ParkingSlotId);
+            return result;
+        }
     }
 }

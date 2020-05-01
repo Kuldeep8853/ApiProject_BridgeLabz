a8fb57baae69e51848fdb0fc7c78b67a8313cb1d
Kuldeep8853 <kkasaudhan8853@gmail.com>
2020-05-01 06:30:53 +0530

    [Kuldeep]Add. Add the police Manager

--- ParkingLot_Problem/Manager/PoliceManager/IPoliceManager.cs
+++ ParkingLot_Problem/Manager/PoliceManager/IPoliceManager.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Manager.PoliceManager
 {
-    interface IPoliceManager
+    public interface IPoliceManager
     {
+        Task<int> Parkking(Parking parking);
+        string UnParking(int parkingSlotId);
     }
 }
--- ParkingLot_Problem/Manager/PoliceManager/PoliceManager.cs
+++ ParkingLot_Problem/Manager/PoliceManager/PoliceManager.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Model;
+using Repository.PoliceRepository;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Manager.PoliceManager
 {
-    class PoliceManager
+    public class PoliceManager : IPoliceManager
     {
+        private readonly IPoliceRepository policeRepository;
+
+        public PoliceManager(IPoliceRepository policeRepository)
+        {
+            this.policeRepository = policeRepository;
+        }
+
+        public Task<int> Parkking(Parking parking)
+        {
+           return this.policeRepository.Parkking(parking);
+        }
+
+        public string UnParking(int parkingSlotId)
+        {
+            return this.policeRepository.UnParking(parkingSlotId);
+        }
     }
 }

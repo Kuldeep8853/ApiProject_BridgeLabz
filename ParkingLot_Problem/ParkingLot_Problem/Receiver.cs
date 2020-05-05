df390387b7a2628d386f6729001c60a0099f9bb0
Kuldeep8853 <kkasaudhan8853@gmail.com>
2020-05-05 06:18:07 +0530

    [Kuldeep]Add. Add the Messaging Service

---
+++ ParkingLot_Problem/ParkingLot_Problem/Receiver.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Experimental.System.Messaging;
+
+namespace ParkingLot_Problem
+{
+    public class Receiver
+    {
+        public void Receive()
+        {
+
+            MessageQueue Msmq = new MessageQueue(@".\Private$\Msmq");
+            List<string> msmqReceive = new List<string>();
+            try
+            {
+                Message[] Messages = Msmq.GetAllMessages();
+
+                if (Messages.Length > 0)
+                {
+                    foreach (Message msg in Messages)
+                    {
+                        msg.Formatter = new XmlMessageFormatter(new string[] { "System.String,mscorlib" });
+                        string Result = msg.Body.ToString();
+                        Msmq.Receive();
+                        msmqReceive.Add(Result);
+                        Msmq.Refresh();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No New Messages in Message Queue");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                TextWriter tw = new StreamWriter(@"");
+
+                foreach (String s in msmqReceive)
+                    tw.WriteLine(s);
+
+                tw.Close();
+                Msmq.Close();
+            }
+
+        }
+    }
+}
---
+++ ParkingLot_Problem/ParkingLot_Problem/Sender.cs
@@ -0,0 +1,29 @@
+﻿using Experimental.System.Messaging;
+using System;
+
+namespace ParkingLot_Problem
+{
+    public class Sender
+    {
+        public void Send(double value)
+        {
+            try
+            {
+                MessageQueue Msmq = null;
+                if (MessageQueue.Exists(@".\Private$\Msmq"))
+                {
+                    Msmq = new MessageQueue(@".\Private$\Msmq");
+                }
+                else
+                {
+                    Msmq = MessageQueue.Create(@".\Private$\Msmq");
+                }
+                Msmq.Send(value.ToString(), "Message send");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
